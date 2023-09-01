84863874c52b973b4a13f567f2770c774f039a48
Thuyet Le <ledangthuyet@gmail.com>
2023-09-01 13:42:34 +0700

    Edit name .js

--- app.js
+++ app.js
@@ -12,65 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const questionField = $("questionField");
     const answerButtons = document.querySelectorAll(".btn-action button");
-    const startBtn = $("start");
-
-    // Khai bao bộ câu hỏi
-    // const questions = [
-    //     {
-    //         question: "What is the closest star to Earth?",
-    //         options: ["The Moon", "Mars", "Venus", "The Sun"],
-    //         correctAnswer: 3,
-    //     },
-    //     {
-    //         question: "Which planet is known as the 'Red Planet'?",
-    //         options: ["Earth", "Venus", "Jupiter", "Mars"],
-    //         correctAnswer: 3,
-    //     },
-    //     {
-    //         question: "What is the largest moon in the solar system?",
-    //         options: ["Ganymede", "Titan", "Io", "The Moon"],
-    //         correctAnswer: 0,
-    //     },
-    //     {
-    //         question:
-    //             "Which planet has the Great Red Spot, a giant storm system?",
-    //         options: ["Neptune", "Saturn", "Jupiter", "Uranus"],
-    //         correctAnswer: 2,
-    //     },
-    //     {
-    //         question: "Which planet is known for its beautiful ring system?",
-    //         options: ["Saturn", "Jupiter", "Mars", "Uranus"],
-    //         correctAnswer: 0,
-    //     },
-    //     {
-    //         question: "What is the smallest planet in our solar system?",
-    //         options: ["Mercury", "Venus", "Earth", "Mars"],
-    //         correctAnswer: 0,
-    //     },
-    //     {
-    //         question:
-    //             "Which planet is often referred to as the 'Evening Star' or 'Morning Star'?",
-    //         options: ["Mars", "Jupiter", "Venus", "Saturn"],
-    //         correctAnswer: 2,
-    //     },
-    //     {
-    //         question:
-    //             "What is the farthest dwarf planet from the Sun in our solar system?",
-    //         options: ["Pluto", "Eris", "Ceres", "Haumea"],
-    //         correctAnswer: 1,
-    //     },
-    //     {
-    //         question:
-    //             "Which planet has the most extensive system of planetary rings?",
-    //         options: ["Saturn", "Jupiter", "Neptune", "Uranus"],
-    //         correctAnswer: 0,
-    //     },
-    //     {
-    //         question: "What is the hottest planet in our solar system?",
-    //         options: ["Mercury", "Venus", "Earth", "Mars"],
-    //         correctAnswer: 1,
-    //     },
-    // ];
+    const startBtn = $("start");   
 
     // Khai bao bien
     let currentCorrectAnswerIndex;
